583e5fc6046569f47992709765ebb32c2c0e0faf
MarcoBousseau <bousseaum@free.fr>
2019-09-19 17:00:34 +0000

    modif finale (normalement)

--- src/Types/exercice2.c
+++ src/Types/exercice2.c
@@ -6,9 +6,11 @@ int main(int argc)
     x0 = 0;
     x1 = 0;
     printf("x0 est en %p, et x1 en %p \n", &x0, &x1);
-    printf("difference : %ld \n", &x0-&x1);
+    printf("difference : %ld \n", &x1-&x0);
     int x2 = 0;
-    printf("difference 2 : %ld \n", &x0-&x2);
+    printf("difference &x2-&x0 : %ld \n", &x2-&x0);
+    printf("difference &x1-&x0 : %ld \n", &x1-&x0);
+    printf("difference &x2-&x1 : %ld \n", &x2-&x1);
     *(&x0 + 1) = 2;
     *(&x0 + 2) = 3;
     printf("nouveau x1 vaut : %d \n", x1);
